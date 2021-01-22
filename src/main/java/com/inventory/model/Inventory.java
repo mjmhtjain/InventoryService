447a5b9c410fa95f8e8e85083e0c300159a9599b
mohitjain@optum.com <mohitjain@optum.com>
2021-01-22 23:13:37 +0530

    init postgres

--- src/main/java/com/inventory/model/Inventory.java
+++ src/main/java/com/inventory/model/Inventory.java
@@ -10,7 +10,7 @@ import org.springframework.data.relational.core.mapping.Table;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table("Inventory")
+@Table("inventory")
 public class Inventory {
 
     @Id

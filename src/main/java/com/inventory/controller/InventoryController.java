e2d45a6b62480ff1bc803a1b4b5aecd578f6242c
mohitjain@optum.com <mohitjain@optum.com>
2021-01-23 12:49:36 +0530

    inventory validation api

--- src/main/java/com/inventory/controller/InventoryController.java
+++ src/main/java/com/inventory/controller/InventoryController.java
@@ -4,10 +4,9 @@ import com.inventory.model.Inventory;
 import com.inventory.repository.InventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/inventory")
@@ -17,8 +16,36 @@ public class InventoryController {
     private InventoryRepository inventoryRepository;
 
     @GetMapping("/find")
-    public Flux<Inventory> fetchAll(){
+    public Flux<Inventory> fetchAll() {
         Flux<Inventory> response = inventoryRepository.findAll();
         return response;
     }
+
+    @GetMapping("/validate/{itemId}/{orderedQuantity}")
+    public Mono<Boolean> validate(@PathVariable int itemId, @PathVariable int orderedQuantity) {
+        Inventory dummyInv = new Inventory(-1, "", -1);
+
+        Mono<Boolean> valid = inventoryRepository
+                .findById(itemId)
+                .switchIfEmpty(Mono.just(dummyInv))
+                .map(inv -> inv.getQuantity())
+                .flatMap(invQuantity -> {
+                    if (invQuantity >= orderedQuantity) {
+                        return Mono.just(true);
+                    } else {
+                        return Mono.just(false);
+                    }
+                });
+
+        return valid;
+    }
+
+//    @GetMapping("/validate/{itemId}")
+//    public Mono<Integer> quantity(@PathVariable int itemId) {
+//        Mono<Integer> invQuantity = inventoryRepository
+//                .findById(itemId)
+//                .map(inv -> inv.getQuantity());
+//
+//        return invQuantity;
+//    }
 }

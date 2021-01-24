8a1ffd701a817c20cbadeed519ae41345290c152
mohitjain@optum.com <mohitjain@optum.com>
2021-01-24 22:55:31 +0530

    commenting connectionfactory

--- src/main/java/com/inventory/InventoryApplication.java
+++ src/main/java/com/inventory/InventoryApplication.java
@@ -25,16 +25,16 @@ public class InventoryApplication {
         SpringApplication.run(InventoryApplication.class, args);
     }
 
-    @Bean
-    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
-        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-        initializer.setConnectionFactory(connectionFactory);
-        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
-        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
-        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("data.sql")));
-        initializer.setDatabasePopulator(populator);
-        return initializer;
-    }
+//    @Bean
+//    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+//        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+//        initializer.setConnectionFactory(connectionFactory);
+//        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+//        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+//        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("data.sql")));
+//        initializer.setDatabasePopulator(populator);
+//        return initializer;
+//    }
 
 }
 

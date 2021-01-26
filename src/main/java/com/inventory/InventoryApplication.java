b4314f83ec69455c0ff013b9a401bf7806f5da82
mohitjain@optum.com <mohitjain@optum.com>
2021-01-26 09:03:59 +0530

    removing the db setup .. pushing that for perf testing

--- src/main/java/com/inventory/InventoryApplication.java
+++ src/main/java/com/inventory/InventoryApplication.java
@@ -19,22 +19,9 @@ import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 @SpringBootApplication
 public class InventoryApplication {
 
-    public static Logger log = LoggerFactory.getLogger(InventoryApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
     }
-
-//    @Bean
-//    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
-//        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-//        initializer.setConnectionFactory(connectionFactory);
-//        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
-//        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
-//        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("data.sql")));
-//        initializer.setDatabasePopulator(populator);
-//        return initializer;
-//    }
 
 }
 
